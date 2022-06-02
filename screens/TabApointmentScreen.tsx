8a7e446be9518bcc7c960d8c81d4d953e4049d24
Gbenge Aondoakula Raphael <gbengeraphael@gmail.com>
2022-06-02 12:03:17 +0100

    init calender

--- screens/TabApointmentScreen.tsx
+++ screens/TabApointmentScreen.tsx
@@ -7,6 +7,7 @@ import { color } from '../constants/Colors';
 import { Image, SliderBase, SliderComponent, StyleSheet, ScrollView } from 'react-native';
 import { FontAwesome, FontAwesome5 } from '@expo/vector-icons';
 import React from 'react';
+import { Calendar } from 'react-native-calendars';
 //import { Calendar } from 'react-native-paper-dates';
 export const Profile1 = ({ style = null, src = null, name = "Aondohemba", sub = "Nurse", participant = "0" }) => {
   return <View style={style ? [styles.item, style] : styles.item}>
@@ -39,13 +40,78 @@ const month = (i: number) => i === 0 ? "Jan" : i === 1 ? "Feb" : i === 2 ? "Marc
 
 export default function TabAppointmentScreen() {
   const date = new Date()
+
+  const showCalender = () => {
+    <Calendar
+      current={date.toString()}
+      // Minimum date that can be selected, dates before minDate will be grayed out. Default = undefined
+      minDate={date.setDate(date.getDate() - 3).toString()}
+      // Maximum date that can be selected, dates after maxDate will be grayed out. Default = undefined
+      maxDate={date.setDate(date.getDate() + 3).toString()}
+      // Handler which gets executed on day press. Default = undefined
+      onDayPress={day => {
+        console.log('selected day', day);
+      }}
+      // Handler which gets executed on day long press. Default = undefined
+      onDayLongPress={day => {
+        console.log('selected day', day);
+      }}
+      // Month format in calendar title. Formatting values: http://arshaw.com/xdate/#Formatting
+      monthFormat={'yyyy MM'}
+      // Handler which gets executed when visible month changes in calendar. Default = undefined
+      onMonthChange={month => {
+        console.log('month changed', month);
+      }}
+      // Hide month navigation arrows. Default = false
+      hideArrows={true}
+      // Replace default arrows with custom ones (direction can be 'left' or 'right')
+      renderArrow={direction => <Arrow />}
+      // Do not show days of other months in month page. Default = false
+      hideExtraDays={true}
+      // If hideArrows = false and hideExtraDays = false do not switch month when tapping on greyed out
+      // day from another month that is visible in calendar page. Default = false
+      disableMonthChange={true}
+      // If firstDay=1 week starts from Monday. Note that dayNames and dayNamesShort should still start from Sunday
+      firstDay={1}
+      // Hide day names. Default = false
+      hideDayNames={true}
+      // Show week numbers to the left. Default = false
+      showWeekNumbers={true}
+      // Handler which gets executed when press arrow icon left. It receive a callback can go back month
+      onPressArrowLeft={subtractMonth => subtractMonth()}
+      // Handler which gets executed when press arrow icon right. It receive a callback can go next month
+      onPressArrowRight={addMonth => addMonth()}
+      // Disable left arrow. Default = false
+      disableArrowLeft={true}
+      // Disable right arrow. Default = false
+      disableArrowRight={true}
+      // Disable all touch events for disabled days. can be override with disableTouchEvent in markedDates
+      disableAllTouchEventsForDisabledDays={true}
+      // Replace default month and year title with custom one. the function receive a date as parameter
+      renderHeader={date => {
+        /*Return JSX*/
+      }}
+      // Enable the option to swipe between months. Default = false
+      enableSwipeMonths={true}
+    />
+  }
+
+
+
+
+
+
+
   return (
     <View style={styles.container}>
 
       <View style={{ width: '100%', flexDirection: 'row', justifyContent: "space-between" }} >
         <View>
-          <View><Text style={[styles.item_sub, { alignItems: "flex-start", alignSelf: 'flex-start' }]} >{month(date.getMonth())} {date.getDate()} {date.getFullYear()}</Text>
+          <View><Text style={[styles.item_sub,
+          { alignItems: "flex-start", alignSelf: 'flex-start' }]} >
+            {month(date.getMonth())} {date.getDate()} {date.getFullYear()}</Text>
             <Text style={styles.title} >Today</Text></View>
+          {showCalender()}
         </View>
         <BtnDefault title={"+ Add"} style={[styles.btn, { borderRadius: 25, paddingHorizontal: 16 }]} />
 
