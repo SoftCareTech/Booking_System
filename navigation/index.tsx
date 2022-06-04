9176d95b03aaef5387943c113142c4e466d104b4
Gbenge Aondoakula Raphael <gbengeraphael@gmail.com>
2022-06-04 06:40:24 +0100

    react first

--- navigation/index.tsx
+++ navigation/index.tsx
@@ -3,12 +3,12 @@
  * https://reactnavigation.org/docs/getting-started
  *
  */
+
+import * as React from 'react';
 import { FontAwesome, FontAwesome5 } from '@expo/vector-icons';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { NavigationContainer, DefaultTheme, DarkTheme } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-Stack';
-import * as React from 'react';
-
 import { ColorSchemeName, Pressable } from 'react-native';
 
 import Colors from '../constants/Colors';
